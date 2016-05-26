63809aaaa414738cd1bd41ca09595a29705c2156
Manoel Amaro <manoel.al.amaro@gmail.com>
2016-05-26 16:34:19 -0300

    Updating code

--- routes/socket.js
+++ routes/socket.js
@@ -3,37 +3,24 @@ module.exports = function(io) {
   var sensors = nano.db.use('sensors');
   var Gpio = require('onoff').Gpio;
   var TempLib = require('node-dht-sensor');
+  var piblaster = require('pi-blaster.js');
 
   var devices = [
     {id: 'sw01', name: 'led01', type: 'switch'},
-    {id: 'sw02', name: 'led02', type: 'switch'},
-    {id: 'sw03', name: 'led03', type: 'switch'},
-    {id: 'sw04', name: 'led04', type: 'switch'},
-    {id: 'sw05', name: 'led05', type: 'switch'},
     {id: 'btn01', name: 'button01', type: 'button'},
-    {id: 'btn02', name: 'button02', type: 'button'},
     {id: 'sensor01', name: 'Temperature/Humidity', type: 'sensor'} 
   ];
 
   var gpios = {
-    'sw01': new Gpio(4, 'out'),
-    'sw02': new Gpio(17, 'out'),
-    'sw03': new Gpio(27, 'out'),
-    'sw04': new Gpio(22, 'out'),
-    'sw05': new Gpio(5,  'out'),
-    'btn01': new Gpio(23, 'in', 'both'),
-    'btn02': new Gpio(24, 'in', 'both'),
+    'sw01': new Gpio(26, 'out'),
+    'btn01': new Gpio(24, 'in', 'both'),
     'sensor01': {port: 25, type: 22, lib: 'dht'}, 
   };
 
   gpios['btn01'].watch(function(err, value) {
     io.emit('update', {button: 'btn01', value: value});
   });
-
-  gpios['btn02'].watch(function(err, value) {
-    io.emit('update', {button: 'btn02', value: value});
-  });
-
+  
   function readSensorData(sensorId, type, port) {
     if (gpios[sensorId].lib == 'dht') {
       var data = TempLib.readSpec(type, port);
@@ -53,6 +40,15 @@ module.exports = function(io) {
       sensors.insert(sensorData, function(err, body) {
         console.log(err);
         io.emit('update_sensor', sensorData);
+        //humidity green
+        piblaster.setPwm(17, (Math.abs((50 - sensorData.data.humidity) / 50) * -1) + 1);
+        //humidity red
+        piblaster.setPwm(4, Math.abs((sensorData.data.humidity - 50) / 50));
+
+        //Temperature red
+        piblaster.setPwm(21, Math.abs((sensorData.data.temperature - 26) / 12));
+        //Temperature green
+        piblaster.setPwm(22, (Math.abs((26 - sensorData.data.temperature) / 12) * -1) + 1);
       });
     }, 60000);
   }
