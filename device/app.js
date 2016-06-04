5aa207cf634f8f0f2fef9a806a820b07f5ccd5f7
Manoel Amaro <mano.lira@gmail.com>
2016-06-04 14:40:57 -0300

    Starts to implement the authentication

--- device/app.js
+++ device/app.js
@@ -1,12 +1,17 @@
 var mqtt = require('mqtt');
 var config = require('./config.json')
 
-var client = mqtt.connect(config.server_url)
+var client = mqtt.connect(config.server_url, {
+  "username": "device",
+  "password": config.device_id
+})
+
+var clientPrefix = '/' + config.client_id + '/';
 
 client.on('connect', function () {
-  client.publish('device_info', JSON.stringify(config.device_info));
+  client.publish(clientPrefix + 'device_info', JSON.stringify(config.device_info));
 });
 
-client.on('message', function (topic, message) {
+client.on(clientPrefix + '/buttons/1', function (topic, message) {
   console.log(message.toString());
 });
--- server/app.js
+++ server/app.js
@@ -1,11 +1,62 @@
 var firebase = require('firebase');
 var firebaseConfig = require('./firebase.json');
+var mosca = require('mosca');
 
 var app = firebase.initializeApp(firebaseConfig);
-
 var auth = firebase.auth();
-var idToken = 'a6rsaiYohnOCw1PPVC93gwG70wG2';  // Get the user's ID token from the client app
+var db = firebase.database();
 
-auth.verifyIdToken(idToken).then(function(decodedToken) {
-  var uid = decodedToken.sub;
+var ref = db.ref("/devices");
+
+var pubsubsettings = {
+  type: 'redis',
+  redis: require('redis'),
+  db: 12,
+  port: 6379,
+  return_buffers: true,
+  host: "localhost"
+};
+
+var moscaSettings = {
+  port: 1883,
+  backend: pubsubsettings
+};
+
+var server = new mosca.Server(moscaSettings);
+server.on('ready', setup);
+
+// Accepts the connection if the username and password are valid
+var authenticate = function(client, username, password, callback) {
+  if (username === 'device') {
+    ref.orderByKey().equalTo(password).once("value", function(snap) {
+      callback(null, true);
+    });
+  }
+}
+
+// In this case the client authorized as alice can publish to /users/alice taking
+// the username from the topic and verifing it is the same of the authorized user
+var authorizePublish = function(client, topic, payload, callback) {
+  callback(null, client.user == topic.split('/')[1]);
+}
+
+// In this case the client authorized as alice can subscribe to /users/alice taking
+// the username from the topic and verifing it is the same of the authorized user
+var authorizeSubscribe = function(client, topic, callback) {
+  callback(null, client.user == topic.split('/')[1]);
+}
+
+function setup() {
+  server.authenticate = authenticate;
+  server.authorizePublish = authorizePublish;
+  server.authorizeSubscribe = authorizeSubscribe;
+  console.log('Mosca server is up and running')
+}
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);
+});
+
+server.on('published', function(packet, client) {
+  console.log('Published', packet.payload);
 });
