06dfb69b27fe6c7c7c3fd77d1a9aa13298ac42f3
Manoel Amaro <mano.lira@gmail.com>
2016-06-04 18:52:39 -0300

    testing publish of device

--- device/app.js
+++ device/app.js
@@ -1,4 +1,5 @@
 var mqtt = require('mqtt');
+var Gpio = require('onoff').Gpio;
 var config = require('./config.json')
 
 var client = mqtt.connect(config.server_url, {
@@ -6,11 +7,20 @@ var client = mqtt.connect(config.server_url, {
   "password": config.device_id
 })
 
-var clientPrefix = '/' + config.client_id + '/';
+var clientPrefix = '/' + config.device_id + '/';
+var buttons = {};
+
+function onButtonPressed(topic, message) {
+
+}
 
 client.on('connect', function () {
+  console.log('connected');
   client.publish(clientPrefix + 'device_info', JSON.stringify(config.device_info));
-  client.on(clientPrefix + '/buttons/1', function (topic, message) {
-    console.log(message.toString());
-  });
+
+  for (var i = 0; i < config.device_info.buttons.length; i++) {
+    buttons[config.device_info.buttons[i].id] = new Gpio(config.device_info.buttons[i].port, 'out');
+    client.on(clientPrefix + 'buttons/' + config.device_info.buttons[i].id, onButtonPressed);
+  }
+
 });
--- server/app.js
+++ server/app.js
@@ -27,11 +27,10 @@ server.on('ready', setup);
 
 // Accepts the connection if the username and password are valid
 var authenticate = function(client, username, password, callback) {
-  console.log(password.toString());
   if (username === 'device') {
     ref.orderByKey().equalTo(password.toString()).once("value", function(snap) {
-      console.log(snap.val());
-      callback(null, snap.val());
+      client.device_id = password.toString();
+      callback(null, true);
     });
   }
 }
@@ -39,13 +38,13 @@ var authenticate = function(client, username, password, callback) {
 // In this case the client authorized as alice can publish to /users/alice taking
 // the username from the topic and verifing it is the same of the authorized user
 var authorizePublish = function(client, topic, payload, callback) {
-  callback(null, client.user == topic.split('/')[1]);
+  callback(null, client.device_id == topic.split('/')[1]);
 }
 
 // In this case the client authorized as alice can subscribe to /users/alice taking
 // the username from the topic and verifing it is the same of the authorized user
 var authorizeSubscribe = function(client, topic, callback) {
-  callback(null, client.user == topic.split('/')[1]);
+  callback(null, client.device_id == topic.split('/')[1]);
 }
 
 function setup() {
@@ -60,5 +59,5 @@ server.on('clientConnected', function(client) {
 });
 
 server.on('published', function(packet, client) {
-  console.log('Published', packet.payload);
+  console.log('Published', packet.payload.toString());
 });
